1a245defa9cf38df9a7988fcb715724dc6ca648e
ChrisSantiago82 <43000085+ChrisSantiago82@users.noreply.github.com>
2026-04-21 10:47:06 -0400

    Update MonitorClass.php

--- src/MonitorClass.php
+++ src/MonitorClass.php
@@ -34,25 +34,10 @@ class MonitorClass
         $mem = explode(" ", $free_arr[1]);
         $mem = array_filter($mem);
         $mem = array_merge($mem);
-        $usedmem = $mem[2];
-        $usedmemInGB = number_format($usedmem / 1048576, 2) . ' GB';
         $memory1 = $mem[2] / $mem[1] * 100;
         $memory = round($memory1, 1);
-        $fh = fopen('/proc/meminfo', 'r');
-        $mem = 0;
-        while ($line = fgets($fh)) {
-            $pieces = array();
-            if (preg_match('/^MemTotal:\s+(\d+)\skB$/', $line, $pieces)) {
-                $mem = $pieces[1];
-                break;
-            }
-        }
-        fclose($fh);
-        $totalram = number_format($mem / 1048576, 2) . ' GB';
 
         $this->results['memoryLoad'] = $memory;
-
-
     }
 
     protected function getCpuLoad()
